3c2b93fdc12c66c2b22506208427de3b75b6b496
System Administrator <root@viveks-Mac-mini-2.local>
2017-02-23 11:42:14 +0530

    save change

--- js/app.js
+++ js/app.js
@@ -5,6 +5,27 @@ app.controller('dashboardController', ['$scope', '$log', function($scope, $log){
 
 
 /*Dashboard Controller*/
-app.controller('adddataController', ['$scope', '$log', function($scope, $log){
-    $scope.test = 'Add Data Page';
+app.controller('adddataController', ['$scope', '$log', '$localStorage','$route','$timeout', function($scope, $log, $localStorage, $route, $timeout){
+    var addObj = [];
+    $scope.addNumber = 0;
+    
+    /*Function for inserting in to Array*/
+    $scope.addUserData = function(){
+        /*Unique id*/        
+        $scope.addNumber++;        
+        addObj.push({id:$scope.addNumber, title:$scope.txtTitle, country:$scope.txtCountry, state:$scope.txtState, city:$scope.txtCity, description:$scope.txtDescription});
+        $localStorage.userDataObj = addObj; 
+        $scope.printUserData = $localStorage.userDataObj;
+        
+    };    
+    
+    /*Temp Delete*/
+    $scope.tempDelete = function(){        
+        $localStorage.$reset({userDataObj : addObj});
+        $scope.printUserData = $localStorage;        
+        $route.reload();
+    };    
+    
+    /*Created Local Storage for Storing Data of User Input*/
+    $scope.printUserData = $localStorage.userDataObj;
 }]);
