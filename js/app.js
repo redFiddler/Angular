172afe52493e9d4c2bcbe6a05af3b683d40a8776
System Administrator <root@viveks-Mac-mini-2.local>
2017-03-01 09:52:08 +0530

    added total files

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ app.controller('dashboardController', ['$scope', '$log', function($scope, $log){
 
 
 /*Dashboard Controller*/
-app.controller('adddataController', ['$scope', '$log', '$localStorage','$route','$timeout', function($scope, $log, $localStorage, $route, $timeout){
+app.controller('adddataController', ['$scope', '$log', '$localStorage','$route','$timeout','$http', function($scope, $log, $localStorage, $route, $timeout, $http){
     var addObj = [];
     $scope.addNumber = 0;
     
@@ -13,7 +13,7 @@ app.controller('adddataController', ['$scope', '$log', '$localStorage','$route',
     $scope.addUserData = function(){
         /*Unique id*/        
         $scope.addNumber++;        
-        addObj.push({id:$scope.addNumber, title:$scope.txtTitle, country:$scope.txtCountry, state:$scope.txtState, city:$scope.txtCity, description:$scope.txtDescription});
+        addObj.push({id:$scope.addNumber, title:$scope.txtTitle, country:$scope.txtCountry.name, state:$scope.txtState.name, city:$scope.txtCity, description:$scope.txtDescription});
         $localStorage.userDataObj = addObj; 
         $scope.printUserData = $localStorage.userDataObj;
         
@@ -28,4 +28,27 @@ app.controller('adddataController', ['$scope', '$log', '$localStorage','$route',
     
     /*Created Local Storage for Storing Data of User Input*/
     $scope.printUserData = $localStorage.userDataObj;
+    
+    //$scope.selectedCountryCode = angular.copy($scope.getCountryData);
+    
+    /*Get Selected Value From Country DropDown*/
+    $scope.getSelectedCountryState = function(){
+        $scope.getCountryValue = $scope.txtCountry.alpha3_code;        
+        /*Getting State Value Depend on Country*/
+        $http.get('http://services.groupkt.com/state/get/'+$scope.getCountryValue+'/all')
+        .then(function(response){
+            $scope.getStateData = response.data;            
+        }, function(response){
+            $scope.getStateData = response.statusText;
+        })
+    };
+    
+    
+    
+    
+    /*Get Country Data From Web Services*/
+    $http.get('http://services.groupkt.com/country/get/all')
+        .then(function(response){
+            $scope.getCountryData = response.data;
+        });
 }]);
